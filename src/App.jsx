3c9d354f595e0266375af1cef131331c1b175480
Behrooz Firoozi <firooziukbehrooz@gmail.com>
2024-08-28 08:55:04 +0100

    add chang4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 
-import {CartProvider} from './context/CartContext'
+
 
 import Navbar from './components/Navbar'
 import Shop from './pages/Shop'
@@ -10,14 +10,14 @@ import Shop from './pages/Shop'
 function App() {
   return (
    //CartProvider  همه مقادیر زیر در زیر مجموعه کارت پروایدر قرار بگیرد 
-    <CartProvider> 
+    
       <Container>
         <Navbar></Navbar>
         <Routes>
           <Route path='/' element={<Shop />} />
         </Routes>
       </Container>
-      </CartProvider> 
+   
   )
 }
 
--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -6,16 +6,15 @@ import { useContext } from 'react'
 import { Card, Button } from 'react-bootstrap'
 /* import profilePic from '../../public/images/speaker.jpg' */
 
-import { CartContext } from '../context/CartContext'
 
                     //prop
 function ProductItem({ product }){
 
   // وقتی برای ما فراخوانی میشه مقدار بازگشتی خواهد داشت اسمش میزارم کارت 
-    const cart = useContext(CartContext)
+   
 
     // این تابع بازگشتی زمانی که کلیک روی دکمه سبد خرید می کنیم استفاده می کنیم
-    const productQuantity = cart.getProductQuantity(product.id)
+    
 
   return(
     <Card className='mt-5 card-bg'> 
@@ -32,18 +31,9 @@ function ProductItem({ product }){
           <Card.Text align='right' className='text-light' dir='rtl'>
             {product.price} تومان
           </Card.Text>
-                   {/*  anonymous function  () => */} 
-                   {/* که مقدار بازگشتیش براساس کانتکس ما باشه */}
-                   {/* آیدی همون محصولی که از طریق کارت کانتکس می خوام به سبد خرید اضافه بشه */}
       
-
-             {/* میام از شرط استفاده می کنم */}
-             {/* در صورتی که بیشتر از صفر بود  */}
-             {/* یک فرگمنت میسازم که بتونم دو تا دکمه درست کنم زیاد و کم کنه */}
-             
-
     
-              <Button onClick={() => cart.addItemToCard(product.id) } variant='btn btn-outline-secondary' className='text-white'>
+              <Button  variant='btn btn-outline-secondary' className='text-white'>
               افزودن به سبد خرید
                 </Button>
         
