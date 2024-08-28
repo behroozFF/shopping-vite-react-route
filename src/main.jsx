754ee918fc56c59d942b7951f3ea1de1bbd970ff
Behrooz Firoozi <firooziukbehrooz@gmail.com>
2024-08-28 09:38:02 +0100

    sss

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import App from './App.jsx'
 
 // اضافه کردن react-bootstrap and  index.css
 import 'bootstrap/dist/css/bootstrap.min.css'
+// فایل های اصلی سی اس اس مون
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
