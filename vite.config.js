fdc8ee390f16ebbb40eb2ccfbd1418235f3cb64c
Behrooz Firoozi <firooziukbehrooz@gmail.com>
2024-08-28 09:45:47 +0100

    dff

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/shopping-vite-react-route"
+  
 })
 
 /* فقط زمانی که می خوای روی گیت هاپ نمایش بدی استفاده کن */
